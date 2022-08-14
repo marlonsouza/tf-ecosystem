0104a5ca56e9d0a7a85b71c4475bda8133372f25
Marlon Souza <marlon.souuza@gmail.com>
2022-08-13 23:01:45 -0300

    Fix names

--- hs-event-scheduler/ecs.tf
+++ hs-event-scheduler/ecs.tf
@@ -16,26 +16,6 @@ resource "aws_ecs_cluster" "event-scheduler-cluster" {
   }
 }
 
-resource "aws_ecs_service" "event-scheduler-service" {
-  name            = "event-scheduler-service"
-  cluster         = aws_ecs_cluster.event-scheduler-cluster.id
-  task_definition = aws_ecs_task_definition.event-scheduler-task.arn
-  desired_count   = 1
-  iam_role        = aws_iam_role.event-scheduler-role.arn
-  depends_on      = [aws_iam_role_policy.event-scheduler-policy, aws_ecs_task_definition.event-scheduler-task]
-
-  ordered_placement_strategy {
-    type  = "binpack"
-    field = "cpu"
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.lb_target.arn
-    container_name   = "${var.environment}-lb-core"
-    container_port   = var.container_port
-  }
-}
-
 resource "aws_ecs_task_definition" "event-scheduler-task" {
   family = "service"
   container_definitions = jsonencode([
@@ -62,6 +42,27 @@ resource "aws_ecs_task_definition" "event-scheduler-task" {
   placement_constraints {
     type       = "memberOf"
     expression = "attribute:ecs.availability-zone in [us-east-1a, us-east-1b]"
+  }
+}
+
+
+resource "aws_ecs_service" "event-scheduler-service" {
+  name            = "event-scheduler-service"
+  cluster         = aws_ecs_cluster.event-scheduler-cluster.id
+  task_definition = aws_ecs_task_definition.event-scheduler-task.arn
+  desired_count   = 1
+  iam_role        = aws_iam_role.event-scheduler-role.arn
+  depends_on      = [aws_iam_role_policy.event-scheduler-policy, aws_ecs_task_definition.event-scheduler-task]
+
+  ordered_placement_strategy {
+    type  = "binpack"
+    field = "cpu"
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.lb_target.arn
+    container_name   = "${var.environment}-lb-core"
+    container_port   = var.container_port
   }
 }
 
